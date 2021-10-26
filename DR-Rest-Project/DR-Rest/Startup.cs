3202280c06c7f8850008c8cd079b54616f84b930
AlexTriumf <TriumfAlex@gmail.com>
2021-10-26 19:36:22 +0200

    update

--- DR-Rest-Project/DR-Rest/Startup.cs
+++ DR-Rest-Project/DR-Rest/Startup.cs
@@ -33,6 +33,16 @@ namespace DR_Rest
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "DR_Rest", Version = "v1" });
             });
+            services.AddCors(options =>
+            {
+              
+                options.AddPolicy("AllowAny",
+                builder => builder.AllowAnyOrigin().
+               AllowAnyMethod().
+               AllowAnyHeader()
+                );
+               
+            });
 
             services.AddDbContext<ModelContext>(opt =>
                 opt.UseSqlServer(MySecret.ConnectionString));
@@ -49,6 +59,8 @@ namespace DR_Rest
             }
 
             app.UseRouting();
+
+            app.UseCors("AllowAny");
 
             app.UseAuthorization();
 
